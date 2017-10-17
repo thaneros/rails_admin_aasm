8543511c07252e3a0a3da4a27faaa60aa046e70a
thaneros <benjamin.durin@wanadoo.fr>
2017-10-18 02:04:26 +0800

    Use Rails 5 redirect_back instead of redirect_to :back

--- lib/rails_admin_aasm/action.rb
+++ lib/rails_admin_aasm/action.rb
@@ -42,7 +42,7 @@ module RailsAdmin
             else
               flash[:error] = I18n.t('admin.state_machine.no_id')
             end
-            redirect_to :back
+            redirect_back
           end
         end
 
